b094af01f3fa064144dc62d1d10b74d484dfa067
Saif <saifisami2005@gmail.com>
2024-09-13 16:49:46 -0400

    Finished implementation of MarkovMachine.

--- markov.js
+++ markov.js
@@ -8,6 +8,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
+    this.chains = {};
     this.makeChains();
   }
 
@@ -17,14 +18,35 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = this.chains;
+    let idx = 0;
+
+    for (let word of this.words) {
+      if (!chains[word]) chains[word] = []; // if the word doesn't exist in the chain itself, add it.
+      if (!idx) { idx++; continue }; // if the id is equal to zero, then skip this iteration as index 0 is the first index, therefore having nothing before it.
+      chains[this.words[idx - 1]].push(word); // this adds the current word in an array, which is associated with the word behind it. (This won't work for index 0, since it's the first.)
+      idx++;
+    }
+
+    chains[this.words[this.words.length - 1]].push(null);
+    return chains;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let stringOfText = "";
+
+    let chosenWord = this.words[Math.floor(Math.random() * this.words.length)]
+
+    for (let i = 0; i < numWords; i++) {
+      stringOfText = stringOfText + " " + chosenWord; //add the word into stringOfText
+      let chooseWordFromChain = this.chains[chosenWord];
+      chosenWord = chooseWordFromChain[Math.floor(Math.random() * chooseWordFromChain.length)];
+      if (!chosenWord) break;
+    }
+    return stringOfText;
   }
 }
 
